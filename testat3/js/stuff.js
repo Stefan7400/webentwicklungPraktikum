e121948799d0287259809be5ac1b1b2df88d0a95
stefangit <lindner634@gmail.com>
2022-11-02 08:41:21 +0100

    right color of border

--- testat3/js/stuff.js
+++ testat3/js/stuff.js
@@ -5,10 +5,36 @@ let confirmPasswordInput = document.getElementById("confirm-pwd");
 
 usernameInput.addEventListener("keyup", e => {
     const currentInput = usernameInput.value
-    console.log(currentInput)
     if(currentInput.length <= 3){
+        usernameInput.style.outline = "none";
         usernameInput.style.borderColor = "red"
     } else {
-        usernameInput.style.borderColor = "gray"
+        usernameInput.style.outline = "none";
+        usernameInput.style.borderColor = "green"
+    }
+    //TODO check if username is already used
+})
+
+passwordInput.addEventListener("keyup", e => {
+    const currentInput = passwordInput.value;
+    if(currentInput.length < 8){
+        passwordInput.style.outline = "none";
+        passwordInput.style.borderColor = "red"
+    } else {
+        passwordInput.style.outline = "none";
+        passwordInput.style.borderColor = "green"
+    }
+})
+
+
+confirmPasswordInput.addEventListener("keyup", e => {
+    const currentInput = confirmPasswordInput.value;
+    const shouldEqual = passwordInput.value;
+    if(currentInput === shouldEqual){
+        confirmPasswordInput.style.outline = "none";
+        confirmPasswordInput.style.borderColor = "green"
+    } else {
+        confirmPasswordInput.style.outline = "none";
+        confirmPasswordInput.style.borderColor = "red"
     }
 })
