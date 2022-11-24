40c6ee167f22d57ad0c4600e9b384dd6d1c5f5b5
stefangit <lindner634@gmail.com>
2022-11-24 10:54:27 +0100

    changes

--- wtp-chat/src/app/components/login/login.component.ts
+++ wtp-chat/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BackendService} from "../../services/backend.service";
 
 @Component({
     selector: 'app-login',
@@ -6,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    authFailed: boolean = false;
+    usernameInput : string = "";
+    passwordInput : string = "";
 
-    public constructor() { 
+
+    public constructor(private backendService : BackendService) {
     }
 
     public ngOnInit(): void {
+    }
+
+    public onLogin(){
+      console.log("YOO")
+      this.backendService.login(this.usernameInput,this.passwordInput).subscribe(data => {
+        this.authFailed = !data;
+      })
     }
 
 }
