f2537e28373cabec918316dc77c1b077675cb08a
julees7 <julia.seehausen@gmx.de>
2022-11-30 22:21:30 +0100

    somewhat wip in settings
    shit is giving me brain pain

--- wtp-chat/src/app/components/settings/settings.component.ts
+++ wtp-chat/src/app/components/settings/settings.component.ts
@@ -11,34 +11,47 @@ import { User } from "../../models/User";
     styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-    
-    currentProfile: Profile = new Profile("Unknown", "User", '1', "Loreleis Opossum Dolores, sie atmet!", "oneLine"); //dummy data
-    firstName: String = this.currentProfile.firstName; 
-    lastName: String = this.currentProfile.lastName; 
-    coffeOrTea: String = this.currentProfile.coffeeOrTea;
-    description: String = this.currentProfile.description;
-    layout: String = this.currentProfile.layout;
-        
-    
-    public constructor(private backendService: BackendService, private contextService: ContextService) {}
+
+    public firstName: string = "";
+    public lastName: string = "";
+    public coffeOrTea: string = "";
+    public description: string = "";
+    public layout: string = "";
+    public currentUserProfile: Profile = new Profile(this.firstName, this.lastName, this.coffeOrTea, this.description, this.layout)
+
+    public constructor(private backendService: BackendService, private contextService: ContextService) { }
 
     public ngOnInit(): void {
-        // TODO: pre set radio button on oneLine and Select on neitherNor aaand enable change again
-        // TODO: load user profile!? and set data accordingly
-        // TODO: if reload profile page dont log out
+        // TODO: radio button on oneLine and Select
+        // TODO: preselect neither nor for coffeOrTea
+        // TODO: load user profile and set data accordingly
+        // TODO: if reload profile page dont log out 
         this.backendService.loadCurrentUser().subscribe((user) => {
             if (user != null) {
                 this.contextService.loggedInUsername = user.username;
-                this.firstName = user.username;
-
-                console.log("Current User is: ", user.username);
+                console.log("Current User is: ", user);
+                // assign values, but cant get values cus subscribe only gives me a User not an ArrayBuffer (which is where all da good stuff is)
             } else {
                 console.log("Current User not found!");
             }
         });
+
     }
 
     public saveProfile(): void {
-        this.backendService.saveCurrentUserProfile(this.currentProfile);
+        // prolly the utmost unefficient way to do this, but my brain be matsch
+        this.currentUserProfile.firstName = this.firstName;
+        this.currentUserProfile.lastName = this.lastName;
+        this.currentUserProfile.coffeeOrTea = this.coffeOrTea;
+        this.currentUserProfile.description = this.description;
+        this.currentUserProfile.layout = this.layout;
+        this.backendService.saveCurrentUserProfile(this.currentUserProfile).subscribe((success) => {
+            if (success) {
+                console.log("saving sucessfull, new data: ", this.currentUserProfile);
+            } else {
+                console.log("error while saving profile");
+                // TODO: (maybe give alert)
+            }
+        });
     }
 }
