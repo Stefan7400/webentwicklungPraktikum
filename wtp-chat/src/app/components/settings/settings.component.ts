4516dc3582f6808f8c815c20ab7bad79dc19cd5b
julees7 <julia.seehausen@gmx.de>
2022-11-30 17:41:51 +0100

    init of settings and profile
    i am slowly but sure losing it :)
    ich brauch ne pause, will be back later um das hier noch mehr zu zersören, denn fixen ...haha ja schön wärs

--- wtp-chat/src/app/components/settings/settings.component.ts
+++ wtp-chat/src/app/components/settings/settings.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BackendService } from "../../services/backend.service";
+import { ContextService } from "../../services/context.service"
+import { Router } from "@angular/router";
+import { Profile } from "../../models/Profile";
+import { User } from "../../models/User";
 
 @Component({
     selector: 'app-settings',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-    public constructor() {
-    }
+    
+    currentProfile: Profile = new Profile("Unknown", "User", '1', "Loreleis Opossum Dolores, sie atmet!", "oneLine"); //dummy data
+    firstName: String = this.currentProfile.firstName; 
+    lastName: String = this.currentProfile.lastName; 
+    coffeOrTea: String = this.currentProfile.coffeeOrTea;
+    description: String = this.currentProfile.description;
+    layout: String = this.currentProfile.layout;
+        
+    
+    public constructor(private backendService: BackendService, private contextService: ContextService) {}
 
     public ngOnInit(): void {
+        // TODO: pre set radio button on oneLine and Select on neitherNor aaand enable change again
+        // TODO: load user profile!? and set data accordingly
+        // TODO: if reload profile page dont log out
+        this.backendService.loadCurrentUser().subscribe((user) => {
+            if (user != null) {
+                this.contextService.loggedInUsername = user.username;
+                this.firstName = user.username;
+
+                console.log("Current User is: ", user.username);
+            } else {
+                console.log("Current User not found!");
+            }
+        });
+    }
+
+    public saveProfile(): void {
+        this.backendService.saveCurrentUserProfile(this.currentProfile);
     }
 }
