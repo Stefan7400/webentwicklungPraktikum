1b6fc863932d53b5407fc5ec77ca0a86f6fda8c1
Lisa <lir9944@thi.de>
2022-12-14 14:12:50 +0100

    finished setup, BackendService works

--- testat5/utils/BackendService.php
+++ testat5/utils/BackendService.php
@@ -1,6 +1,9 @@
 <?php
 	namespace Utils;
 
+	use Model\User;
+	use Model\Friend;
+
 	class BackendService {
 		private $base;
 		private $id;
@@ -10,38 +13,115 @@
 			$this->id = $id;
 		}
 
-		public function test() {
+		public function login($username, $password) {
 			try {
-				return HttpClient::get($this->base . '/test.json');
+				$data = HttpClient::post($this->base . "/" . $this->id . "/login",
+											array("username" => $username, "password" => $password));
+				$_SESSION['chatToken'] = $data->token;
+				return true;
 			} catch(\Exception $e) {
-				echo "Authentification failed" . $e;
+				error_log($e);
 			}
 			return false;
 		}
 
-		public function login($username, $password) {
-			try {
-				$result = HttpClient::post($this->base . "/" . $this->id . "/login",
-											array("username" => $username, "password" => $password));
-				// $_SESSION['chatToken'] = $result->token;
-				echo "login ok<br>";
-				return true;
-			} catch(\Exception $e) {
-				echo "login error: " . $e . "<br>";
-				return false;
-			}
-		}
-
 		public function register($username, $password) {
 			try {
-				$result = HttpClient::post($this->base . "/" . $this->id . "/register",
-					array("username" => $username, "password" => $password));
-				//echo "Token: " . $result->token;
-				$_SESSION['token'] = $result->token; // save for subsequent calls
-				return true;
+				if(!$this->userExists($username)) {
+					$data = HttpClient::post($this->base . "/" . $this->id . "/register",
+												array("username" => $username, "password" => $password));
+					$_SESSION['chatToken'] = $data->token; // save for subsequent calls
+					return true;
+				}
 			} catch(\Exception $e) {
-				echo "register error: " . $e . "<br>";
-				return false;
+				error_log($e);
 			}
+			return false;
+		}
+
+		public function loadUser($username) {
+			try {
+				$data = HttpClient::get($this->base . "/" . $this->id . "/user/" . $username, $_SESSION['chatToken']);
+				return User::fromJson($data);
+			} catch (\Exception $e) {
+				error_log($e);
+			}
+			return null;
+		}
+
+		public function saveUser($user) {
+			try {
+				return HttpClient::post($this->base . "/" . $this->id . "/user/" . $user->getUsername(), $user, $_SESSION['chatToken']);
+			} catch (\Exception $e) {
+				error_log($e);
+			}
+			return false;
+		}
+
+		public function loadFriends() {
+			try {
+				$data = HttpClient::get($this->base . "/" . $this->id . "/friend", $_SESSION['chatToken']);
+				return Friend::fromJson($data);
+			} catch (\Exception $e) {
+				error_log($e);
+			}
+			return null;
+		}
+
+		public function friendRequest($friend) {
+			try {
+				return HttpClient::post($this->base . "/" . $this->id . "/friend", $friend, $_SESSION['chatToken']);
+			} catch (\Exception $e) {
+				error_log($e);
+			}
+			return false;
+		}
+
+		public function friendAccept($friend) {
+			try {
+				return HttpClient::put($this->base . "/" . $this->id . "/friend/" . $friend->getUsername(), $friend, $_SESSION['chatToken']);
+			} catch (\Exception $e) {
+				error_log($e);
+			}
+			return false;
+		}
+
+		public function friendDismiss($friend) {
+			try {
+				return HttpClient::put($this->base . "/" . $this->id . "/friend/" . $friend->getUsername(), $friend, $_SESSION['chatToken']);
+			} catch (\Exception $e) {
+				error_log($e);
+			}
+			return false;
+		}
+
+		public function friendRemove($friend) {
+			try {
+				return HttpClient::delete($this->base . "/" . $this->id . "/friend/" . $friend->getUsername(), $_SESSION['chatToken']);
+			} catch (\Exception $e) {
+				error_log($e);
+			}
+			return false;
+		}
+
+		public function userExists($username) {
+			try {
+				$result = HttpClient::get($this->base . "/" . $this->id . "/user/" . $username, $_SESSION['chatToken']);
+				if(isset($result->username)) {
+					return true;
+				}
+			} catch (\Exception $e) {
+				error_log($e);
+			}
+			return false;
+		}
+
+		public function getUnread() {
+			try {
+				return (array) HttpClient::get($this->base . "/" . $this->id . "/unread", $_SESSION['chatToken']);
+			} catch (\Exception $e) {
+				error_log($e);
+			}
+			return null;
 		}
 	}
